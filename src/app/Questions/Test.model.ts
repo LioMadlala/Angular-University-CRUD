7e31359b1a6e7364d22285bc89485c08c6a1e88b
william <william@thedigitalacademy.co.za>
2020-09-13 14:07:17 +0200

    question ts added

---
+++ src/app/Questions/Test.model.ts
@@ -0,0 +1,221 @@
+import { Question } from "./qustions.interface";
+
+//http://www.allindiaexams.in/engineering/cse/typescript-mcq-quiz-typescript-online-test
+//https://www.code-sample.com/2019/04/angular-8-7-6-5-4-2-online-test-free.html
+var test: Question[] = [
+  {
+    question: "Which are the different Data Types supported by Typescript?",
+    answers: [
+      {
+        position: "A",
+        text: "Boolean var bValue: boolean = false",
+      },
+      {
+        position: "B",
+        text: "Number var age: number = 16",
+      },
+      {
+        position: "C",
+        text: 'String var name: string = "jon"',
+      },
+      {
+        position: "D",
+        text: "All of the above mentioned",
+      },
+    ],
+  },
+  {
+    question: "Which statements are true regarding JSX modes?",
+    answers: [
+      {
+        position: "A",
+        text:
+          "These modes only affect the emit stage - type checking is unaffected",
+      },
+      {
+        position: "B",
+        text: "The output of react mode will have a .jsx file extension",
+      },
+      {
+        position: "C",
+        text: "The react mode will emit React.addElement",
+      },
+      {
+        position: "D",
+        text:
+          "he preserve mode will keep the JSX as part of the output to be further consumed by another transform step",
+      },
+    ],
+  },
+  {
+    question: " What are variables in Typescript?",
+    answers: [
+      {
+        position: "A",
+        text: "A variable name should contain alphabets and numeric digits",
+      },
+      {
+        position: "B",
+        text:
+          "It cannot contain spaces and special characters except underscore (_) and dollar ($) sig",
+      },
+      {
+        position: "C",
+        text: " A variable name cannot begin with a digit",
+      },
+      {
+        position: "D",
+        text: "All",
+      },
+    ],
+  },
+  {
+    question: "What are the components of TypeScript?",
+    answers: [
+      {
+        position: "A",
+        text: "TypeScript Language",
+      },
+      {
+        position: "B",
+        text: "TypeScript Compiler",
+      },
+      {
+        position: "C",
+        text: "TypeScript Language Service",
+      },
+      {
+        position: "D",
+        text: "All of these",
+      },
+    ],
+  },
+  {
+    question:
+      " What are the types of access modifiers supported by TypeScript?",
+    answers: [
+      {
+        position: "A",
+        text: " Public",
+      },
+      {
+        position: "B",
+        text: "Private",
+      },
+      {
+        position: "C",
+        text: " Protected",
+      },
+      {
+        position: "D",
+        text: "All of these",
+      },
+    ],
+  },
+  {
+    question: "When was the first time TypeScript was made public?",
+    answers: [
+      {
+        position: "A",
+        text: "December 2012",
+      },
+      {
+        position: "B",
+        text: "October 2012",
+      },
+      {
+        position: "C",
+        text: "October 2013",
+      },
+      {
+        position: "D",
+        text: "November 2013",
+      },
+    ],
+  },
+  {
+    question: "Who is sometimes called as father of Angularjs?",
+    answers: [
+      {
+        position: "A",
+        text: "Brad Green",
+      },
+      {
+        position: "B",
+        text: "Misko Hevery",
+      },
+      {
+        position: "C",
+        text: "Brad Richardson",
+      },
+      {
+        position: "D",
+        text: "Chuck Jazdzewski",
+      },
+    ],
+  },
+  {
+    question: "What is the decorator used for configuring your module class?",
+    answers: [
+      {
+        position: "A",
+        text: "@NgModule",
+      },
+      {
+        position: "B",
+        text: "@NgApp",
+      },
+      {
+        position: "C",
+        text: "Both",
+      },
+      {
+        position: "D",
+        text: "None of above",
+      },
+    ],
+  },
+  {
+    question:
+      "Which angular decorator allows us to define the pipe name that is globally available for use in any template in the across application?",
+    answers: [
+      {
+        position: "A",
+        text: "pipeName",
+      },
+      {
+        position: "B",
+        text: "@pipeDeco",
+      },
+      {
+        position: "C",
+        text: "Pipe",
+      },
+      {
+        position: "D",
+        text: "None of the above",
+      },
+    ],
+  },
+  {
+    question: "Which character is used for chaining multiple pipes?",
+    answers: [
+      {
+        position: "A",
+        text: "|",
+      },
+      {
+        position: ":",
+        text: "@pipeDeco",
+      },
+      {
+        position: "C",
+        text: "Both",
+      },
+      {
+        position: "D",
+        text: "None of above",
+      },
+    ],
+  },
+];
---
+++ src/app/Questions/qustions.interface.ts
@@ -0,0 +1,6 @@
+export class Question{
+  question?:string;
+  answers?:any[];
+}
+
+
